191bb2c48667a7caea8c2a79b1eb49917d497f64
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-04-26 19:58:09 -0300

    Fiz bug firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
     authDomain: process.env.AUTH_DOMAIN,
     projectId: process.env.PROJECT_ID,
     storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSASING_SENDER_ID,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
     appId: process.env.APP_ID,
     measurementId: process.env.MEASUREMENT_ID
   };
